b8ae0a6eb68cfccae70ad6c0c32972d2e4591234
wannaBeAyan <enriquez7adrian@gmail.com>
2023-11-08 20:42:19 +0800

    Fixed Everything
    Fixed Everything

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import CharCounter from './CharCounter.jsx'
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CharCounter />
+    <App />
   </React.StrictMode>,
 )
